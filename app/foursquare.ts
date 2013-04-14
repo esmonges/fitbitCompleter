85427d7034351ca4f7632b895a1e39f3462c2b1b
Omer Zach <omer.zach@gmail.com>
2013-04-14 16:30:08 -0400

    Random style

--- app/foursquare.ts
+++ app/foursquare.ts
@@ -6,8 +6,6 @@ import express = module("express");
 var foursquare = require("../node_modules/temboo/Library/Foursquare/Venues");
 
 export function initFoursquare(app) {
-//  app.post(express.bodyParser());
-
   app.get("/foursquare-venues", (request, response) => {
     var searchVenuesChoreo = new foursquare.SearchVenues(temboo.session);
 
@@ -17,7 +15,6 @@ export function initFoursquare(app) {
     var query = data.query;
     var latitude = data.latitude;
     var longitude = data.longitude;
-
 
     // Set credential to use for execution
     searchVenuesInputs.set_ClientID("2UBT5JSQ53KR4BPB3KXZCIK2OSTXRQNA2N2SQC322LGVKDNA");
--- app/googlewalkingdistance.ts
+++ app/googlewalkingdistance.ts
@@ -3,25 +3,23 @@ import express = module("express");
 
 var Google = require("temboo/Library/Google/DistanceMatrix");
 
-export function initGoogleWalkingDistance(app){
-  app.get("/google-walking-distance", function(request, response){
+export function initGoogleWalkingDistance(app) {
+  app.get("/google-walking-distance", function(request, response) {
     var walkingDistanceMatrixChoreo = new Google.WalkingDistanceMatrix(temboo.session);
 
     var walkingDistanceMatrixInputs = walkingDistanceMatrixChoreo.newInputSet();
-console.log(request.query);
+    console.log(request.query);
     walkingDistanceMatrixInputs.set_Destinations(request.query.queries);
     walkingDistanceMatrixInputs.set_Origins(request.query.origins);
 
     walkingDistanceMatrixChoreo.execute(
       walkingDistanceMatrixInputs,
-      function(results){
+      results => {
         console.log(results.get_Response());
-        response.send({success:true, results: results.get_Response()});
+        response.send({success: true, results: results.get_Response()});
       },
-      function(error){
-        response.send({success: false, error: error});
-      }
+      error => response.send({ success: false, error: error })
     );
-
   });
 }
+
