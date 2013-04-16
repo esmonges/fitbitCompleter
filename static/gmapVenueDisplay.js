ab9a25b99d3025136d20eb939aba49355d3ff5b4
Omer Zach <omer.zach@gmail.com>
2013-04-16 14:30:04 -0400

    Style, display proper step number
    You were converting from meters to miles and then things got weird

--- static/gmapVenueDisplay.js
+++ static/gmapVenueDisplay.js
@@ -1,68 +1,70 @@
-  /* TODO: 
-   * dynamic determination of zoom based on marker location
-   * create links to directions (probably in fsvenues js code)
-   */
+/* TODO:
+ * dynamic determination of zoom based on marker location
+ * create links to directions (probably in fsvenues js code)
+ */
 
-  function initGmap(lat, lng){
-    var mapDiv = $("#map-canvas");
-    mapDiv.removeClass("hiddenMap");
-    mapDiv.addClass("visibleMap");
+function initGmap(lat, lng) {
+  var mapDiv = $("#map-canvas");
+  mapDiv.removeClass("hiddenMap");
+  mapDiv.addClass("visibleMap");
 
-    var mapOptions ={
-      center: new google.maps.LatLng(lat, lng),
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    g.map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
+  var mapOptions = {
+    center: new google.maps.LatLng(lat, lng),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  g.map = new google.maps.Map(
+    document.getElementById("map-canvas"),
+    mapOptions
+  );
 
-    g.centerMarker = new google.maps.Marker({
-      position: g.map.getCenter(),
-      map: g.map,
-      title: "You are here"
-    });
+  g.centerMarker = new google.maps.Marker({
+    position: g.map.getCenter(),
+    map: g.map,
+    title: "You are here"
+  });
 
-    console.log(g.map);
+  console.log(g.map);
+}
+
+function markerHandler(event) {
+  var visibles = $(".visible");
+  visibles.removeClass("visible");
+  visibles.addClass("hidden");
+
+  var i = event.target.id;
+
+  var link = $(event.target).children();
+
+  link.removeClass("hidden");
+  link.addClass("visible");
+
+  var lat = g.displayedVenues[i].venue.location.lat;
+  var lng = g.displayedVenues[i].venue.location.lng;
+  putMarker(lat, lng);
+}
+
+function putMarker(lat, lng) {
+  if (g.destMarker) {
+    g.destMarker.setMap(null);
   }
 
-  function markerHandler(event){
-    var visibles = $(".visible");
-    visibles.removeClass("visible");
-    visibles.addClass("hidden");
+  g.destMarker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: g.map,
+    title: "Destiniation"
+  });
+}
 
-    var i = event.target.id;
+function generateDirectionsFromFSV(FSvenue) {
+  var saddr = "";
+  var daddr = "";
 
-    var link = $(event.target).children();
+  saddr = g.lat + "," + g.lon;
+  daddr = FSvenue.location.lat + "," + FSvenue.location.lng;
 
-    link.removeClass("hidden");
-    link.addClass("visible");
+  return "https://maps.google.com/maps?saddr=" + saddr
+         + "&daddr=" + daddr
+         + "&dirflg=w";
+}
 
-    var lat = g.displayedVenues[i].venue.location.lat;
-    var lng = g.displayedVenues[i].venue.location.lng;
-    putMarker(lat, lng);
-  }
-
-  function putMarker(lat, lng){
-
-    if(g.destMarker){
-      g.destMarker.setMap(null);
-    }
-
-    g.destMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat,lng),
-      map: g.map,
-      title: "Destiniation"
-    });
-  }
-
-  function generateDirectionsFromFSV(FSvenue){
-    var saddr = "";
-    var daddr = "";
-
-    saddr = g.lat + "," + g.lon;
-    daddr = FSvenue.location.lat + "," + FSvenue.location.lng;
-
-    return "https://maps.google.com/maps?saddr=" + saddr
-           + "&daddr=" + daddr 
-           + "&dirflg=w";
-  }
--- static/stepFinder.js
+++ static/stepFinder.js
@@ -1,194 +1,196 @@
-var g = {KM_TO_MI: 0.621371,
-           lat: 0, 
-           lon: 0, 
-           NSUGGESTIONS: 5,
-           displayedVenues: []};
+var g = {
+  KM_TO_MI: 0.621371,
+  lat: 0,
+  lon: 0,
+  NSUGGESTIONS: 5,
+  displayedVenues: []
+};
 
 
-  function getGeoAndSubmit(){
-    navigator.geolocation.getCurrentPosition(submitFSVSearch);
-  }
+function getGeoAndSubmit() {
+  navigator.geolocation.getCurrentPosition(submitFSVSearch);
+}
 
-  function submitFSVSearch(position){
-    var fsvQuery = $("#fsvQuery");
-    var query = fsvQuery.val();
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    g['lat'] = lat;
-    g['lon'] = lon;
+function submitFSVSearch(position) {
+  var fsvQuery = $("#fsvQuery");
+  var query = fsvQuery.val();
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  g['lat'] = lat;
+  g['lon'] = lon;
 
-    fsvQuery.val("");
+  fsvQuery.val("");
 
-    $.ajax({
-      type:"get",
-      data: {query: query, 
-        latitude: lat, 
-        longitude: lon},
-        url:"/foursquare-venues",
-        success: function(data){
-          var results = JSON.parse(data.results);
-          var venues = results.response.venues;
-          getWalkingDistances(venues);
-          initGmap(lat, lon);}
-        });
-  }
-
-  // function displayVenuesAndDistances(venues, distObjs){
-  //   data.results = JSON.parse(data.results);
-  //   var venues = data.results.response.venues;
-  //   var MAXENTRIES = 5;
-  //   var i;
-
-  //   var nEntries = Math.min(venues.length, MAXENTRIES);
-
-  //   var sortedVenues = venues.sort(function(a,b){
-  //     return a.location.distance - b.location.distance;
-  //   });
-
-  //   g.sortedVenues = sortedVenues;
-
-  //   getAndDisplayWalkingDistance(sortedVenues.slice(0,nEntries+1))
-  // }
-
-  function pairSortAndStore(venues, distObjs){
-
-///////REMOVE THESE LINES////////
-localStorage.stepsPerMile = 1;
-localStorage.remainingSteps = 2000;
-///////REMOVE THESE LINES////////
-
-    var targetDist = (localStorage.remainingSteps/localStorage.stepsPerMile)/2;
-    var pairedList = [];
-    var i;
-
-
-    for(i=0; i < venues.length; i++){
-      pairedList[i] = {};
-      pairedList[i].venue = venues[i];
-      pairedList[i].distanceInMiles = distObjs[i].distance.value * g.KM_TO_MI;
-      pairedList[i].distanceInSteps = Math.round(pairedList[i].distanceInMiles * localStorage.stepsPerMile);
+  $.ajax({
+    type:"get",
+    data: {
+      query: query,
+      latitude: lat,
+      longitude: lon
+    },
+    url:"/foursquare-venues",
+    success: function(data) {
+      var results = JSON.parse(data.results);
+      var venues = results.response.venues;
+      getWalkingDistances(venues);
+      initGmap(lat, lon);
     }
+  });
+}
 
-    sortedPL = pairedList.sort(function(a,b){
-      var adiff = Math.abs(a.distanceInMiles - targetDist);
-      var bdiff = Math.abs(b.distanceInMiles - targetDist);
-      return adiff - bdiff;
-    });
+// function displayVenuesAndDistances(venues, distObjs) {
+//   data.results = JSON.parse(data.results);
+//   var venues = data.results.response.venues;
+//   var MAXENTRIES = 5;
+//   var i;
 
-    g.sortedPL = sortedPL;
+//   var nEntries = Math.min(venues.length, MAXENTRIES);
+
+//   var sortedVenues = venues.sort(function(a, b) {
+//     return a.location.distance - b.location.distance;
+//   });
+
+//   g.sortedVenues = sortedVenues;
+
+//   getAndDisplayWalkingDistance(sortedVenues.slice(0, nEntries + 1))
+// }
+
+function pairSortAndStore(venues, distObjs) {
+  var targetDist = (localStorage.remainingSteps / localStorage.stepsPerMile) / 2;
+  var pairedList = [];
+  var i;
+
+  for (i = 0; i < venues.length; i++) {
+    pairedList[i] = {};
+    pairedList[i].venue = venues[i];
+    pairedList[i].distanceInMiles = (distObjs[i].distance.value / 1000) * g.KM_TO_MI;
+    console.log(distObjs[i].distance);
+    pairedList[i].distanceInSteps = Math.round(pairedList[i].distanceInMiles * localStorage.stepsPerMile);
   }
 
-  function getWalkingDistances(venues){
-    var queries = "";
-    var origins;
-    var i = 0;
+  sortedPL = pairedList.sort(function(a, b) {
+    var adiff = Math.abs(a.distanceInMiles - targetDist);
+    var bdiff = Math.abs(b.distanceInMiles - targetDist);
+    return adiff - bdiff;
+  });
 
-    for(i = 0; i < venues.length; i++){
-      queries = queries + venues[i].location.lat + ","
-      + venues[i].location.lng + "|";
-    }
+  g.sortedPL = sortedPL;
+}
 
-    queries = queries.substring(0, queries.length - 1);
+function getWalkingDistances(venues) {
+  var queries = "";
+  var origins;
+  var i = 0;
 
-    origins = g.lat + "," + g.lon;
+  for (i = 0; i < venues.length; i++) {
+    queries = queries + venues[i].location.lat + ","
+              + venues[i].location.lng + "|";
+  }
 
-    $.ajax({
-      type:"get",
-      data:{queries:queries, origins:origins, units:"imperial"},
-      url:"/google-walking-distance",
-      success: function(data){
-        var results = JSON.parse(data.results);
-        var distObjs = results.rows[0].elements;
-        pairSortAndStore(venues, distObjs);
+  queries = queries.substring(0, queries.length - 1);
 
-        displaySuggestionsStartingAt(0);
+  origins = g.lat + "," + g.lon;
+
+  $.ajax({
+    type: "get",
+    data: {
+      queries: queries,
+      origins: origins,
+      units:"imperial"
+    },
+    url:"/google-walking-distance",
+    success: function(data) {
+      var results = JSON.parse(data.results);
+      var distObjs = results.rows[0].elements;
+      pairSortAndStore(venues, distObjs);
+
+      displaySuggestionsStartingAt(0);
 /*        var resultsDiv = $("#results");
         resultsDiv.html("");
-        for(i = 0; i < venues.length; i++){
+        for(i = 0; i < venues.length; i++) {
           var newDiv = $("<li>");
           newDiv.attr("id", i);
           newDiv.onButtonTap(markerHandler);
-          newDiv.html(venues[i].name + ", Steps: " 
+          newDiv.html(venues[i].name + ", Steps: "
             + results.rows[0].elements[i].distance.text);
           resultsDiv.append(newDiv);
         } */
-      }
-    });
-  }
-
-  function displaySuggestionsStartingAt(index){
-    var end = Math.min(index + g.NSUGGESTIONS, g.sortedPL.length);
-    var i;
-    var gdvIndex = 0;
-    var resultsDiv = $("#suggestions");
-
-    resultsDiv.html("");
-    g.displayedVenues = [];
-
-    for(i = index; i < end; i++){
-      g.displayedVenues[gdvIndex++] = g.sortedPL[i];
-      displaySuggestion(g.sortedPL[i], i);
     }
+  });
+}
+
+function displaySuggestionsStartingAt(index) {
+  var end = Math.min(index + g.NSUGGESTIONS, g.sortedPL.length);
+  var i;
+  var gdvIndex = 0;
+  var resultsDiv = $("#suggestions");
+
+  resultsDiv.html("");
+  g.displayedVenues = [];
+
+  for (i = index; i < end; i++) {
+    g.displayedVenues[gdvIndex++] = g.sortedPL[i];
+    displaySuggestion(g.sortedPL[i], i);
   }
+}
 
-  function displaySuggestion(s, i){
-    var resultsDiv = $("#suggestions");
-    var newDiv = $("<li>");
-    var dirLink = $("<a>");
-    var link;
+function displaySuggestion(s, i) {
+  var resultsDiv = $("#suggestions");
+  var newDiv = $("<li>");
+  var dirLink = $("<a>");
+  var link;
 
-    newDiv.attr("id", i);
-    newDiv.onButtonTap(markerHandler);
+  newDiv.attr("id", i);
+  newDiv.onButtonTap(markerHandler);
 
-    link = generateDirectionsFromFSV(s.venue);
-    dirLink.attr("href", link);
-    dirLink.html("Get Directions");
-    dirLink.addClass("hidden");
+  link = generateDirectionsFromFSV(s.venue);
+  dirLink.attr("href", link);
+  dirLink.html("Get Directions");
+  dirLink.addClass("hidden");
 
-    newDiv.html(s.venue.name + ", Steps (Each Way): " 
-      + s.distanceInSteps);
-    newDiv.append(dirLink);
-    resultsDiv.append(newDiv);
+  newDiv.html(s.venue.name + ", Steps (Each Way): " + s.distanceInSteps);
+  newDiv.append(dirLink);
+  resultsDiv.append(newDiv);
 
-  }
+}
 
 
-// function getAndDisplayWalkingDistance(venues){
-  //   var queries = "";
-  //   var origins;
-  //   var i = 0;
+// function getAndDisplayWalkingDistance(venues) {
+//   var queries = "";
+//   var origins;
+//   var i = 0;
 
-  //   g.displayedVenues = venues;
-    
-  //   for(i = 0; i < venues.length; i++){
-  //     queries = queries + venues[i].location.lat + ","
-  //     + venues[i].location.lng + "|";
-  //   }
+//   g.displayedVenues = venues;
 
-  //   queries = queries.substring(0, queries.length - 1);
+//   for(i = 0; i < venues.length; i++) {
+//     queries = queries + venues[i].location.lat + ","
+//     + venues[i].location.lng + "|";
+//   }
 
-  //   origins = g.lat + "," + g.lon;
+//   queries = queries.substring(0, queries.length - 1);
 
-  //   $.ajax({
-  //     type:"get",
-  //     data:{queries:queries, 
-  //       origins:origins},
-  //       url:"/google-walking-distance",
-  //       success: function(data){
-  //         var results = JSON.parse(data.results);
-  //         var resultsDiv = $("#results");
-  //         resultsDiv.html("");
-  //         for(i = 0; i < venues.length; i++){
-  //           var newDiv = $("<li>");
+//   origins = g.lat + "," + g.lon;
 
-  //           newDiv.attr("id", i);
-  //           newDiv.onButtonTap(markerHandler);
+//   $.ajax({
+//     type:"get",
+//     data:{queries:queries,
+//       origins:origins},
+//       url:"/google-walking-distance",
+//       success: function(data) {
+//         var results = JSON.parse(data.results);
+//         var resultsDiv = $("#results");
+//         resultsDiv.html("");
+//         for(i = 0; i < venues.length; i++) {
+//           var newDiv = $("<li>");
 
-  //           newDiv.html(venues[i].name + ", Steps: " 
-  //             + results.rows[0].elements[i].distance.text);
-  //           resultsDiv.append(newDiv);
-  //         }
-  //       }
-  //     });
+//           newDiv.attr("id", i);
+//           newDiv.onButtonTap(markerHandler);
 
-  // }
+//           newDiv.html(venues[i].name + ", Steps: "
+//             + results.rows[0].elements[i].distance.text);
+//           resultsDiv.append(newDiv);
+//         }
+//       }
+//     });
+
+// }
