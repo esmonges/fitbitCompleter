1627a31575ed0bf0535a0c5284b42700f2deeefc
Omer Zach <omer.zach@gmail.com>
2013-04-28 21:17:36 -0400

    Add server code for logging activities

--- app/fitbitExercises.ts
+++ app/fitbitExercises.ts
@@ -30,6 +30,32 @@ export function init(server) {
       }
     );
   });
-}
 
+  server.put("/log-fitbit-exercise", (request, response) => {
+    var logActivityChoreo = new Fitbit.LogActivity(session);
+    var logActivityInputs = logActivityChoreo.newInputSet();
+
+    logActivitiesInputs.set_AccessToken(request.query["accessToken"]);
+    logActivitiesInputs.set_AccessTokenSecret(request.query["accessTokenSecret"]);
+    logActivitiesInputs.set_ConsumerKey("63678ae84a134e38ad62a70d473a7d57");
+    logActivitiesInputs.set_ConsumerSecret("f9f4cfc32cc14ad6bc97057d3000fab2");
+    logActivitiesInputs.set_ActivityID(request.body.activityId);
+    logActivitiesInputs.set_Date(request.body.date); // TODO: Just use current date?
+    if (request.body.distance !== undefined) /* TODO */ {
+      logActivitiesInputs.set_Distance(request.body.distance);
+    }
+    logActivitiesInputs.set_DistanceUnit("Mile");
+    logActivitiesInputs.set_Duration(request.body.seconds * 1000);
+    logActivitiesInputs.set_StartTime(request.body.startTime); // TODO: Careful to get formatting right
+
+    logActivitiesChoreo.execute(
+      logActivityInputs,
+      results => response.send({ success: true }),
+      error => {
+        console.log("Failed to log activity from Fitbit");
+        response.send({ success: false, error: error });
+      }
+    );
+  });
+}
 
