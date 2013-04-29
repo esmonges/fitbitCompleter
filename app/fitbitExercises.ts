786c96b45be7449a65d204745525b485ca2eaea8
Omer Zach <omer.zach@gmail.com>
2013-04-29 17:31:56 -0400

    Logging activities almost works
    Getting a weird HTTPS error still from Temboo

--- app/fitbitExercises.ts
+++ app/fitbitExercises.ts
@@ -3,6 +3,28 @@ import express = module("express");
 
 var fitbit = require("../node_modules/temboo/Library/Fitbit/Activities");
 
+// TODO: Consolidate with other identical functions!
+var getCurrentFitbitDate = (): string => {
+  var today = new Date();
+  var day;
+  if (today.getDate() < 10) {
+    day = "0" + (today.getDate().toString());
+  } else {
+    day = today.getDate().toString();
+  }
+
+  var month;
+  if ((today.getMonth() + 1) < 10) {
+    month = "0" + (today.getMonth()).toString();
+  } else {
+    month = (today.getMonth() + 1).toString();
+  }
+
+  var year = today.getFullYear().toString();
+  return "" + year + "-" + month + "-" + day + "/1m"; // TODO
+}
+
+
 export function init(server) {
   server.get("/get-fitbit-exercises", (request, response) => {
     var browseActivitiesChoreo = new fitbit.BrowseActivities(temboo.session);
@@ -33,24 +55,27 @@ export function init(server) {
     var logActivityChoreo = new fitbit.LogActivity(temboo.session);
     var logActivityInputs = logActivityChoreo.newInputSet();
 
-    logActivityInputs.set_AccessToken(request.query["accessToken"]);
-    logActivityInputs.set_AccessTokenSecret(request.query["accessTokenSecret"]);
+    logActivityInputs.set_AccessToken(request.body.accessToken);
+    logActivityInputs.set_AccessTokenSecret(request.body.accessTokenSecret);
     logActivityInputs.set_ConsumerKey("63678ae84a134e38ad62a70d473a7d57");
     logActivityInputs.set_ConsumerSecret("f9f4cfc32cc14ad6bc97057d3000fab2");
     logActivityInputs.set_ActivityID(request.body.activityId);
-    logActivityInputs.set_Date(request.body.date); // TODO: Just use current date?
-    if (request.body.distance !== undefined) /* TODO */ {
+    logActivityInputs.set_Date(getCurrentFitbitDate());
+    /*if (request.body.distance !== undefined) {
       logActivityInputs.set_Distance(request.body.distance);
     }
-    logActivityInputs.set_DistanceUnit("Mile");
-    logActivityInputs.set_Duration(request.body.seconds * 1000);
+    logActivityInputs.set_DistanceUnit("Mile");*/
+    logActivityInputs.set_Duration(request.body.duration * 60 * 1000);
     logActivityInputs.set_StartTime(request.body.startTime); // TODO: Careful to get formatting right
+
+    console.log(request.body);
+    console.log(logActivityInputs);
 
     logActivityChoreo.execute(
       logActivityInputs,
       results => response.send({ success: true }),
       error => {
-        console.log("Failed to log activity from Fitbit");
+        console.log("Failed to log activity to Fitbit");
         response.send({ success: false, error: error });
       }
     );
