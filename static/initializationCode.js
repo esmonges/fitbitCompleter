1830cc7631e77c68d2f5eb68ccd7131918ec1f77
Omer Zach <omer.zach@gmail.com>
2013-04-30 15:11:42 -0400

    Only allow one logout screen

--- static/initializationCode.js
+++ static/initializationCode.js
@@ -6,11 +6,14 @@ function popupLogout() {
   var source = $("#logout-template").html();
   var template = Handlebars.compile(source);
   var data = {};
-  $("body").append(template(data));
-  $("#close-overlay").onButtonTap(
-    closeLogoutPopup,
-    function() {}
-  );
+
+  if ($("#overlay").length === 0) {
+    $("body").append(template(data));
+    $("#close-overlay").onButtonTap(
+      closeLogoutPopup,
+      function() {}
+    );
+  }
 }
 
 function logout() {
