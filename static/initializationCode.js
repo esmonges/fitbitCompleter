b1c773094928eeba4a8afc1f9d8ad2f297a0e0ef
Edward Smogneski <esmonges@andrew.cmu.edu>
2013-04-29 21:52:52 -0400

    logout finished, loading, logincheck

--- static/initializationCode.js
+++ static/initializationCode.js
@@ -15,8 +15,56 @@ function popupLogout(){
 
 function logout(){
   console.log("logout");
+  window.localStorage.clear();
+  closeLogoutPopup();
+  window.location.href = "./signin.html";
 }
 
 function closeLogoutPopup(){
   $("#overlay").remove();
+}
+
+function openLoadingPopup(){
+  var source = $("#loading-template").html();
+  var template = Handlebars.compile(source);
+  var data = {};
+  $("body").append(template(data));
+}
+
+function closeLoadingPopup(){
+  $("#overlay").remove();
+}
+
+function checkLoginBeforeAjax(){
+  if(!loggedInBeforeAjax()){
+    window.location.href = "./signin.html";
+  }
+  else{
+    //should be good...
+  }
+}
+
+function loggedInBeforeAjax(){
+  return ((localStorage["fitbitCompleter" + "oauthTokenSecret"] !== undefined)
+        &&(localStorage["fitbitCompleter" + "callbackId"] !== undefined));
+}
+
+function checkLogin(){
+  if(!loggedIn()){
+    console.log("errrror");
+    $("#load-error").html("Error, please log in to Fitbit");
+    window.setTimeout(function(){
+      window.location.href = "./signin.html";
+    },2000);
+    return false;
+  }
+  else{
+    //good to go!
+    return true;
+  }
+}
+
+function loggedIn(){
+  return ((localStorage["fitbitCompleter" + "AccessToken"] !== undefined)
+        &&(localStorage["fitbitCompleter" + "AccessTokenSecret"] !== undefined));
 }
